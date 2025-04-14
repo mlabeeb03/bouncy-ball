8d9cd1b521bf3695611263543ca0212cbd6a60d2
Muhammad Labeeb <mlabeeb03@gmail.com>
2025-04-14 20:49:09 +0500

    add horizontal ball movement

--- src/main.c
+++ src/main.c
@@ -4,18 +4,20 @@
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
-#define PIXEL_X 1280		// Screen width
-#define PIXEL_Y 800			// Screen height
-#define BALL_SIZE 40		// Radius
-#define BALL_GROUNDED_Y 760 // Y cord for ball on ground (PIXEL_Y - BALL_SIZE)
+#define PIXEL_X 1280			// Screen width
+#define PIXEL_Y 800				// Screen height
+#define BALL_SIZE 40			// Radius
+#define BALL_GROUNDED_Y 760 	// Y cord for ball on ground (PIXEL_Y - BALL_SIZE)
+#define BALL_LEFT_LIMIT 40 		// X cord for ball on left edge (BALL_SIZE)
+#define BALL_RIGHT_LIMIT 1240	// X cord for ball on right edge (PIXEL_X - BALL_SIZE)
 #define JUMP_STRENGTH -20
 
-void update_ball_location(int *ball_x, int *ball_y, int *ball_velocity) {
-	if (*ball_y < BALL_GROUNDED_Y || *ball_velocity < 0) {
-		(*ball_velocity)++;
-		*ball_y = MIN(BALL_GROUNDED_Y, *ball_y + *ball_velocity);
+void update_vertical_location(int *ball_y, int *ball_velocity_y) {
+	if (*ball_y < BALL_GROUNDED_Y || *ball_velocity_y < 0) {
+		(*ball_velocity_y)++;
+		*ball_y = MIN(BALL_GROUNDED_Y, *ball_y + *ball_velocity_y);
 	} else {
-		*ball_velocity = 0;
+		*ball_velocity_y = 0;
 	}
 }
 
@@ -25,7 +27,7 @@ int main() {
 	InitWindow(PIXEL_X, PIXEL_Y, "cgame");
 
 	int ball_x = PIXEL_X / 2, ball_y = BALL_GROUNDED_Y - 400;
-	int ball_velocity = 0;
+	int ball_velocity_y = 0, ball_velocity_x;
 
 	SetTargetFPS(60);
 
@@ -34,11 +36,19 @@ int main() {
 
 		ClearBackground(RAYWHITE);
 
-		if (IsKeyPressed(KEY_SPACE)) {
-			ball_velocity = JUMP_STRENGTH;
+		// Control horizontal movement
+		if (IsKeyDown(KEY_LEFT)){
+			ball_x = MAX(BALL_LEFT_LIMIT, ball_x - 8);
+		}
+		if (IsKeyDown(KEY_RIGHT)){
+			ball_x = MIN(BALL_RIGHT_LIMIT, ball_x + 8);
 		}
 
-		update_ball_location(&ball_x, &ball_y, &ball_velocity);
+		// Control vertical movement
+		if (IsKeyPressed(KEY_SPACE)) {
+			ball_velocity_y = JUMP_STRENGTH;
+		}
+		update_vertical_location(&ball_y, &ball_velocity_y);
 
 		DrawCircle(ball_x, ball_y, BALL_SIZE, RED);
 
