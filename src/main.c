f3f6c8186a091b2301ec14596dbe5e52efa1938e
Muhammad Labeeb <mlabeeb03@gmail.com>
2025-04-18 23:07:01 +0500

    use rectangular platform as floor

--- src/main.c
+++ src/main.c
@@ -7,25 +7,38 @@
 #define PIXEL_X 1280 // Screen width
 #define PIXEL_Y 800	 // Screen height
 #define BALL_SIZE 40 // Radius
-#define BALL_GROUNDED (PIXEL_Y - BALL_SIZE)
 #define BALL_LEFT_LIMIT (BALL_SIZE)
 #define BALL_RIGHT_LIMIT (PIXEL_X - BALL_SIZE)
-#define JUMP_STRENGTH -25
+#define JUMP_STRENGTH -25 // Lower number means a higher jump
 
 typedef struct {
-	int x, y, size, jump_strength, velocity;
+	float x, y, size, jump_strength, velocity;
 } Ball;
 
-void update_vertical_location(Ball *ball) {
-	if ((*ball).y < BALL_GROUNDED || (*ball).velocity < 0) {
+typedef struct {
+	float x, y, width, height;
+} Platform;
+
+int ball_bottom(Ball *ball) { return (*ball).y + (*ball).size; }
+
+int ball_on_platform(Ball *ball, Platform *platform) {
+	Vector2 center = {.x = (*ball).x, .y = (*ball).y};
+	Rectangle rectangle = *(Rectangle *)platform;
+	if (CheckCollisionCircleRec(center, (*ball).size, rectangle)) {
+		return 1;
+	}
+	return 0;
+}
+
+void update_vertical_location(Ball *ball, Platform *platform) {
+	if (!ball_on_platform(ball, platform) || (*ball).velocity < 0) {
 		((*ball).velocity)++;
-		(*ball).y = MIN(BALL_GROUNDED, (*ball).y + (*ball).velocity);
+		(*ball).y =
+			MIN((*platform).y - (*ball).size, (*ball).y + (*ball).velocity);
 	} else {
 		(*ball).velocity = 0;
 	}
 }
-
-int ball_on_ground(Ball *ball) { return (*ball).y == BALL_GROUNDED ? 1 : 0; }
 
 int main() {
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
@@ -33,10 +46,13 @@ int main() {
 	InitWindow(PIXEL_X, PIXEL_Y, "cgame");
 
 	Ball ball = {.x = PIXEL_X / 2,
-				 .y = BALL_GROUNDED,
+				 .y = PIXEL_Y / 2,
 				 .size = BALL_SIZE,
 				 .jump_strength = JUMP_STRENGTH,
 				 .velocity = 0};
+
+	Platform platform = {
+		.x = 0, .y = PIXEL_Y - 20, .width = PIXEL_X, .height = 20};
 
 	SetTargetFPS(60);
 
@@ -44,6 +60,9 @@ int main() {
 		BeginDrawing();
 
 		ClearBackground(RAYWHITE);
+
+		DrawRectangle(platform.x, platform.y, platform.width, platform.height,
+					  BLACK);
 
 		// Control horizontal movement
 		if (IsKeyDown(KEY_LEFT)) {
@@ -54,10 +73,10 @@ int main() {
 		}
 
 		// Control vertical movement
-		if (ball_on_ground(&ball) && IsKeyPressed(KEY_SPACE)) {
+		if ((ball_on_platform(&ball, &platform)) && IsKeyPressed(KEY_SPACE)) {
 			ball.velocity = JUMP_STRENGTH;
 		}
-		update_vertical_location(&ball);
+		update_vertical_location(&ball, &platform);
 
 		DrawCircle(ball.x, ball.y, ball.size, RED);
 
